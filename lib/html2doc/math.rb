5e94855c363635d506feb6ee32d4e6797f475656
Nick Nicholas <opoudjis@gmail.com>
2021-05-05 21:52:49 +1000

    rubocop

--- lib/html2doc/math.rb
+++ lib/html2doc/math.rb
@@ -9,20 +9,20 @@ module Html2Doc
     Nokogiri::XSLT(File.read(File.join(File.dirname(__FILE__), "mml2omml.xsl"),
                              encoding: "utf-8"))
 
-  def self.asciimath_to_mathml1(x)
-    begin
-      AsciiMath::MathMLBuilder.new(:msword => true).append_expression(
-        AsciiMath.parse(HTMLEntities.new.decode(x)).ast).to_s.
-      gsub(/<math>/, "<math xmlns='http://www.w3.org/1998/Math/MathML'>")
-    rescue StandardError => e
-      puts "parsing: #{x}"
-      puts e.message
-      raise e
-    end
+  def self.asciimath_to_mathml1(expr)
+    AsciiMath::MathMLBuilder.new(msword: true).append_expression(
+      AsciiMath.parse(HTMLEntities.new.decode(expr)).ast,
+    ).to_s
+      .gsub(/<math>/, "<math xmlns='http://www.w3.org/1998/Math/MathML'>")
+  rescue StandardError => e
+    puts "parsing: #{expr}"
+    puts e.message
+    raise e
   end
 
   def self.asciimath_to_mathml(doc, delims)
     return doc if delims.nil? || delims.size < 2
+
     m = doc.split(/(#{Regexp.escape(delims[0])}|#{Regexp.escape(delims[1])})/)
     m.each_slice(4).map.with_index do |(*a), i|
       i % 500 == 0 && m.size > 1000 && i > 0 and
@@ -42,81 +42,86 @@ module Html2Doc
   end
 
   # random fixes to MathML input that OOXML needs to render properly
-  def self.ooxml_cleanup(m, docnamespaces)
-    m = unwrap_accents(mathml_preserve_space(
-      mathml_insert_rows(m, docnamespaces), docnamespaces))
-    m.add_namespace(nil, "http://www.w3.org/1998/Math/MathML")
-    m
+  def self.ooxml_cleanup(math, docnamespaces)
+    math = unwrap_accents(
+      mathml_preserve_space(
+        mathml_insert_rows(math, docnamespaces), docnamespaces
+      ),
+    )
+    math.add_namespace(nil, "http://www.w3.org/1998/Math/MathML")
+    math
   end
 
-  def self.mathml_insert_rows(m, docnamespaces)
-    m.xpath(%w(msup msub msubsup munder mover munderover).
-            map { |m| ".//xmlns:#{m}" }.join(" | "), docnamespaces).each do |x|
+  def self.mathml_insert_rows(math, docnamespaces)
+    math.xpath(%w(msup msub msubsup munder mover munderover)
+            .map { |m| ".//xmlns:#{m}" }.join(" | "), docnamespaces).each do |x|
       next unless x.next_element && x.next_element != "mrow"
+
       x.next_element.wrap("<mrow/>")
     end
-    m
+    math
   end
 
-  def self.mathml_preserve_space(m, docnamespaces)
-    m.xpath(".//xmlns:mtext", docnamespaces).each do |x|
+  def self.mathml_preserve_space(math, docnamespaces)
+    math.xpath(".//xmlns:mtext", docnamespaces).each do |x|
       x.children = x.children.to_xml.gsub(/^\s/, "&#xA0;").gsub(/\s$/, "&#xA0;")
     end
-    m
+    math
   end
 
-  def self.unitalic(m)
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'p']]").each do |x|
+  def self.unitalic(math)
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'p']]").each do |x|
       x.wrap("<span style='font-style:normal;'></span>")
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'bi']]").each do |x|
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'bi']]").each do |x|
       x.wrap("<span class='nostem' style='font-weight:bold;'><em></em></span>")
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'i']]").each do |x|
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'i']]").each do |x|
       x.wrap("<span class='nostem'><em></em></span>")
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'b']]").each do |x|
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:scr)]/xmlns:sty[@m:val = 'b']]").each do |x|
       x.wrap("<span style='font-style:normal;font-weight:bold;'></span>")
     end
-    m.xpath(".//xmlns:r[xmlns:rPr/xmlns:scr[@m:val = 'monospace']]").each do |x|
-      toPlane1(x, :monospace)
+    math.xpath(".//xmlns:r[xmlns:rPr/xmlns:scr[@m:val = 'monospace']]").each do |x|
+      to_plane1(x, :monospace)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr/xmlns:scr[@m:val = 'double-struck']]").each do |x|
-      toPlane1(x, :doublestruck)
+    math.xpath(".//xmlns:r[xmlns:rPr/xmlns:scr[@m:val = 'double-struck']]").each do |x|
+      to_plane1(x, :doublestruck)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'script']]").each do |x|
-      toPlane1(x, :script)
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'script']]").each do |x|
+      to_plane1(x, :script)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'script']]").each do |x|
-      toPlane1(x, :scriptbold)
+    math.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'script']]").each do |x|
+      to_plane1(x, :scriptbold)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'fraktur']]").each do |x|
-      toPlane1(x, :fraktur)
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'fraktur']]").each do |x|
+      to_plane1(x, :fraktur)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'fraktur']]").each do |x|
-      toPlane1(x, :frakturbold)
+    math.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'fraktur']]").each do |x|
+      to_plane1(x, :frakturbold)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
-      toPlane1(x, :sans)
+    math.xpath(".//xmlns:r[xmlns:rPr[not(xmlns:sty) or xmlns:sty/@m:val = 'p']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
+      to_plane1(x, :sans)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
-      toPlane1(x, :sansbold)
+    math.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'b']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
+      to_plane1(x, :sansbold)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'i']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
-      toPlane1(x, :sansitalic)
+    math.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'i']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
+      to_plane1(x, :sansitalic)
     end
-    m.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'bi']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
-      toPlane1(x, :sansbolditalic)
+    math.xpath(".//xmlns:r[xmlns:rPr[xmlns:sty/@m:val = 'bi']/xmlns:scr[@m:val = 'sans-serif']]").each do |x|
+      to_plane1(x, :sansbolditalic)
     end
-    m
+    math
   end
 
-  def self.toPlane1(x, font)
-    x.traverse do |n|
+  def self.to_plane1(xml, font)
+    xml.traverse do |n|
       next unless n.text?
+
       n.replace(Plane1Converter.conv(HTMLEntities.new.decode(n.text), font))
     end
-    x
+    xml
   end
 
   def self.mathml_to_ooml(docxml)
@@ -126,22 +131,23 @@ module Html2Doc
       i % 100 == 0 && m.size > 500 && i > 0 and
         warn "Math OOXML #{i} of #{m.size}"
       element = ooxml_cleanup(x, docnamespaces)
-      doc = Nokogiri::XML::Document::new()
+      doc = Nokogiri::XML::Document::new
       doc.root = element
-      ooxml = (unitalic(esc_space(@xsltemplate.transform(doc)))).to_s.
-        gsub(/<\?[^>]+>\s*/, "").
-        gsub(/ xmlns(:[^=]+)?="[^"]+"/, "").
-        gsub(%r{<(/)?(?!span)(?!em)([a-z])}, "<\\1m:\\2")
+      ooxml = unitalic(esc_space(@xsltemplate.transform(doc))).to_s
+        .gsub(/<\?[^>]+>\s*/, "")
+        .gsub(/ xmlns(:[^=]+)?="[^"]+"/, "")
+        .gsub(%r{<(/)?(?!span)(?!em)([a-z])}, "<\\1m:\\2")
       ooxml = uncenter(x, ooxml)
       x.swap(ooxml)
     end
   end
 
-  # escape space as &#x32;; we are removing any spaces generated by 
+  # escape space as &#x32;; we are removing any spaces generated by
   # XML indentation
   def self.esc_space(xml)
     xml.traverse do |n|
       next unless n.text?
+
       n = n.text.gsub(/ /, "&#x32;")
     end
     xml
@@ -149,17 +155,15 @@ module Html2Doc
 
   # if oomml has no siblings, by default it is centered; override this with
   # left/right if parent is so tagged
-  def self.uncenter(m, ooxml)
-    if m.next == nil && m.previous == nil
-      alignnode = m.at(".//ancestor::*[@style][local-name() = 'p' or "\
-                       "local-name() = 'div' or local-name() = 'td']/@style")
-      return ooxml unless alignnode
-      if alignnode.text.include? ("text-align:left")
+  def self.uncenter(math, ooxml)
+    alignnode = math.at(".//ancestor::*[@style][local-name() = 'p' or "\
+                     "local-name() = 'div' or local-name() = 'td']/@style")
+    return ooxml unless alignnode && (math.next == nil && math.previous == nil)
+
+    %w(left right).each do |dir|
+      if alignnode.text.include? ("text-align:#{dir}")
         ooxml = "<m:oMathPara><m:oMathParaPr><m:jc "\
-          "m:val='left'/></m:oMathParaPr>#{ooxml}</m:oMathPara>"
-      elsif alignnode.text.include? ("text-align:right")
-        ooxml = "<m:oMathPara><m:oMathParaPr><m:jc "\
-          "m:val='right'/></m:oMathParaPr>#{ooxml}</m:oMathPara>"
+          "m:val='#{dir}'/></m:oMathParaPr>#{ooxml}</m:oMathPara>"
       end
     end
     ooxml
