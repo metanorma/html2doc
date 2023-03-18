aab45f2aa6c8ae85fda4a2e29bad06f4cdcda785
Nick Nicholas <opoudjis@gmail.com>
2023-03-18 13:43:27 +1100

    cosmetic

--- lib/html2doc/math.rb
+++ lib/html2doc/math.rb
@@ -6,9 +6,6 @@ require "plane1converter"
 
 class Html2Doc
   def asciimath_to_mathml1(expr, retain_asciimath)
-    #AsciiMath::MathMLBuilder.new(msword: true).append_expression(
-      #AsciiMath.parse(HTMLEntities.new.decode(expr)).ast,
-    #).to_s
     ret = Plurimath::Math.parse(HTMLEntities.new.decode(expr), "asciimath").to_mathml
       .gsub(/<math>/, "<math xmlns='http://www.w3.org/1998/Math/MathML'>")
     retain_asciimath and
