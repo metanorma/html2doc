a08c4cf5362af3079813e126090e4c3c2ff6cf91
Nick Nicholas <opoudjis@gmail.com>
2023-09-05 19:31:44 +1000

    preserve @id on v:rect v:line, instead of moving it to <a name>: https://github.com/metanorma/metanorma-ieee/issues/4

--- lib/html2doc/base.rb
+++ lib/html2doc/base.rb
@@ -141,7 +141,7 @@ class Html2Doc
     docxml.xpath("//*[@id][not(@name)][not(@style = 'mso-element:footnote')]")
       .each do |x|
       (x["id"].empty? ||
-        %w(v:shapetype v:shape v:rect v:line).include?(x.name)) and next
+        %w(v:shapetype v:shape v:rect v:line v:group).include?(x.name)) and next
       if x.children.empty? then x.add_child("<a name='#{x['id']}'></a>")
       else x.children.first.previous = "<a name='#{x['id']}'></a>"
       end
