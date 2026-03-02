c56402ae4179779833ba72dff7ece6757aff2a9d
Nick Nicholas <opoudjis@gmail.com>
2026-03-02 11:53:55 +1100

    canon gem

--- html2doc.gemspec
+++ html2doc.gemspec
@@ -49,4 +49,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rubocop-performance"
   spec.add_development_dependency "simplecov", "~> 0.15"
   spec.add_development_dependency "timecop", "~> 0.9"
+  spec.add_development_dependency "canon", ">= 0.1.13"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,21 @@ require "rspec/match_fuzzy"
 require "html2doc"
 require "rspec/matchers"
 require "equivalent-xml"
+require "canon"
+
+Canon::Config.instance.tap do |cfg|
+  # Configure Canon to use spec-friendly match profiles
+  cfg.xml.match.profile = :spec_friendly
+  cfg.html.match.profile = :spec_friendly
+
+  # Configure Canon to show all diffs (including inactive diffs)
+  cfg.html.diff.show_diffs = :all
+  cfg.xml.diff.show_diffs = :all
+
+  # Enable verbose diff output for debugging
+  cfg.html.diff.verbose_diff = true
+  cfg.xml.diff.verbose_diff = true
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
